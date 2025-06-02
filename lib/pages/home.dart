67221ff40f5890c067d5f9028c271ce3367fe4c4
Temuulen <temvvlen1211@gmail.com>
2025-06-02 19:47:27 +0800

    test

---
+++ lib/pages/home.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {return Scaffold(appBar: AppBar(title: Text("gg",style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: ),),))}
+}
